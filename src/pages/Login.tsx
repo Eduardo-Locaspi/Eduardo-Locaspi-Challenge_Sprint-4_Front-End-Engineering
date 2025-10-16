f3c0959f39330d64d12512530a8a08a48645c28b
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-10-16 18:26:24 -0300

    Base para Tela de Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+export default function Login(){
+
+    return(
+        <div className="bg-red-400 min-h-screen  flex ">
+            
+            <div className="bg-white w-3/12 m-auto rounded-md p-10">
+                 <h1 className="text-4xl font-sans text-center mb-2">Teste Login</h1>
+                 <p>fdsfs</p>
+            </div>
+            
+            
+        </div>
+    )
+}
