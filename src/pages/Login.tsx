04b69cce08d436f60784725cf697746cb9daef6d
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-10-16 23:59:44 -0300

    Conteudo Finalizado da Página de Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,23 @@
 export default function Login(){
 
     return(
-        <div className="bg-red-400 min-h-screen  flex ">
+        <div className="bg-red-800 min-h-screen  flex ">
             
-            <div className="bg-white w-3/12 m-auto rounded-md p-10">
-                 <h1 className="text-4xl font-sans text-center mb-2">Teste Login</h1>
-                 <p>fdsfs</p>
+            <div className="bg-white w-3/12 m-auto rounded-md p-10 flex flex-col items-center">
+                <h1 className="text-4xl font-sans text-center mb-10">Faça o seu Login</h1>
+                
+                <form action="" className="flex flex-col gap-5 items-center">
+                
+                    <input className="outline-2 rounded-md w-full px-2.5 py-3 text-2xl " type="text" placeholder="login" />
+
+                    <input className="outline-2 rounded-md w-full px-2.5 py-3 text-2xl" type="text" placeholder="senha" />
+
+                    <button className="bg-red-800 text-white w-6/12 text-2xl py-2 rounded-full my-5" >Entrar</button>
+                </form>
+
+                
+                
+                <button className="text-red-800 bg-gray-200 w-6/12 text-2xl py-2 rounded-full my-5 border-2 border-red-800" >Cadastrar</button>
             </div>
             
             
