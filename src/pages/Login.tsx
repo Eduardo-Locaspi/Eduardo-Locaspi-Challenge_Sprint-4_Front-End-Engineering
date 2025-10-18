7512bb683b8f84d700b9267efb74a396a565c4f0
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-10-18 12:17:09 -0300

    Tipagem do usuario no login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import CampoDeLogin from "../components/CampoDeLogin";
+import FormLogin from "../components/FormLogin";
 
 export default function Login(){
     // Oq falta? - Analisar se o paciente exise na banco de dados
@@ -12,8 +12,9 @@ export default function Login(){
             <div className="bg-white w-3/12 m-auto rounded-md p-10 flex flex-col items-center">
                 <h1 className="text-4xl font-sans text-center mb-10">Faça o seu Login</h1>
                 
-                <CampoDeLogin/>
-                
+                <FormLogin/>
+
+                <p>Não tem uma conta?</p>
                 <button className="text-red-800 bg-gray-200 w-6/12 text-2xl py-2 rounded-full my-5 border-2 border-red-800" >Cadastrar</button>
             </div>
             
